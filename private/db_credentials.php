e552910d701e05ce839ea95b4d1c3c7920209468
rojrun <runroj@gmail.com>
2022-05-10 16:59:39 -0700

    initial commit

---
+++ private/db_credentials.php
@@ -0,0 +1,6 @@
+<?php
+define("DB_SERVER", "localhost");
+define("DB_USER", "gbank");
+define("DB_PASS", "8Malv!n0");
+define("DB_NAME", "globe_bank");
+?>
---
+++ public/staff/pages/edit.php
@@ -0,0 +1,75 @@
+<?php 
+require_once('../../../private/initialize.php');
+
+if(!isset($_GET['id'])) {
+  redirect_to(url_for('/staff/pages/index.php'));
+}
+$id = $_GET['id'];
+
+if(is_post_request()) {
+  // Handle form values sent by new.php
+  $page = [];
+  $page['subject_id'] = $_POST['subject_id'] ?? '';
+  $page['menu_name'] = $_POST['menu_name'] ?? '';
+  $page['position'] = $_POST['position'] ?? '';
+  $page['visible'] = $_POST['visible'] ?? '';
+  $page['content'] = $_POST['content'] ?? '';
+
+  $result = update_page($page);
+  redirect_to(url_for('/staff/pages/show.php?id=' . $id));
+} else {
+  $page = find_page_by_id($id);
+  $page_set = find_all_pages();
+  $page_count = mysqli_num_rows($page_set);
+  mysqli_free_result($page_set);
+}
+?>
+
+<?php $page_title = 'Edit Page'; ?>
+<?php include(SHARED_PATH . '/staff_header.php'); ?>
+
+<div id="content">
+
+  <a class="back-link" href="<?php echo url_for('/staff/pages/index.php'); ?>">&laquo; Back to List</a>
+
+  <div class="page edit">
+    <h1>Edit Page</h1>
+
+    <form action="<?php echo url_for('/staff/pages/edit.php?id=' . h(u($id))); ?>" method="post">
+      <dl>
+        <dt>Menu Name</dt>
+        <dd><input type="text" name="menu_name" value="<?php echo h($menu_name); ?>" /></dd>
+      </dl>
+      <dl>
+        <dt>Position</dt>
+        <dd>
+          <select name="position">
+            <?php 
+              for($i=1; $i <= $page_count; $i++) {
+                echo "<option value=\"{$i}\"";
+                if($page["position"] == $i) {
+                  echo " selected";
+                }
+                echo ">{$i}</option>";
+              }
+            ?>
+          </select>
+        </dd>
+      </dl>
+      <dl>
+        <dt>Visible</dt>
+        <dd>
+          <input type="hidden" name="visible" value="0" />
+          <input type="checkbox" name="visible" value="1"<?php if($visible == "1") { echo " checked"; } ?> />
+        </dd>
+      </dl>
+      <div id="operations">
+        <input type="submit" value="Edit Subject" />
+      </div>
+    </form>
+
+  </div>
+
+</div>
+
+<?php include(SHARED_PATH . '/staff_footer.php'); ?>
